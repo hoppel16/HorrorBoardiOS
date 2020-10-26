6bfc053d4d91b829b2e01420b359a8655cf9b169
Hunter Oppel <hunter.oppel@gmail.com>
2020-10-26 15:39:42 -0400

    Initial Commit

---
+++ HorrorBoard/HorrorBoard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HorrorBoard
+//
+//  Created by Hunter Oppel on 10/26/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
